b62f753832093ef1a735953e461b6194712b9246
Alex Mendes <alexander.h.mendes@gmail.com>
2024-03-18 13:06:09 +0000

    refactor: fix linting issues in release config

--- release.config.js
+++ release.config.js
@@ -1,22 +1,22 @@
 module.exports = {
-  branches: [{ name: 'main' }],
+  branches: [{ name: "main" }],
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/github',
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
     [
-      '@semantic-release/npm',
+      "@semantic-release/npm",
       {
         npmPublish: false,
       },
     ],
     [
-      '@semantic-release/git',
+      "@semantic-release/git",
       {
-        assets: ['package.json'],
+        assets: ["package.json"],
         message:
           // eslint-disable-next-line no-template-curly-in-string
-          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
   ],
